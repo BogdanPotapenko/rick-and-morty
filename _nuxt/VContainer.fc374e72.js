41965645daa7877784a3b90ef09830536f4d44e7
unknown <21bogdan09@gmail.com>
2023-03-11 14:17:08 +0200

    Updates

---
+++ _nuxt/VContainer.fc374e72.js
@@ -0,0 +1,1 @@
+import{m as t,u as n}from"./VProgressCircular.616b4f9a.js";import{s,j as r}from"./entry.27f97aed.js";const f=s({name:"VContainer",props:{fluid:{type:Boolean,default:!1},...t()},setup(e,a){let{slots:o}=a;return n(()=>r(e.tag,{class:["v-container",{"v-container--fluid":e.fluid}]},o)),{}}});export{f as V};
